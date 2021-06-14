64d2e2ebb9db1ad61a44ac84699dd67dab03e363
Selfie SD <83945969+Selfie-bd@users.noreply.github.com>
2021-06-14 14:27:16 +0530

    Update script.py

--- script.py
+++ script.py
@@ -26,17 +26,12 @@ No need add each filter again!
 Bot will automatically search for your file and give links to that!
 </i>
 
-<b>© @Mo_Tech_YT</b>"""
-
-
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot</b>
+<b>© @Movies_Telegram_File</b>"""
+    ABOUT_Me"""⭕️<b>My Name : Auto Filter Bot</b>
     
-⭕️<b>Creater :</b> @MRK_YT
+⭕️<b>Creater :</b> @selfiebd
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/d_oX4b7ucWM'>Video Link</a> 
-
 """
