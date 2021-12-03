eaf38e27676c682621388ccb542130ee8726aa15
Selfie SD <webtamil0@gmail.com>
2021-12-03 10:26:24 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,22 +1,24 @@
 class script(object):
 
 
-    START_MSG = """ <b>Hi {} I Am @flimrequest Auto Filter Bot 🤖
+    START_MSG = """ <b>Hi {} I Am Auto Filter Bot 🤖
+
+I am Private Bot 🤖 To this Channel 👉 @ThiruttuRockers4k
 
 For info click <i>help</i></b>"""
 
 
     HELP_MSG = """<b>How to use the bot??</b>
 <i>    
-* Message to @selfiebd
+* I am Private Use only My Channel 👉 @ThiruttuRockers4k
 </i>
 
-<b>〽️ Powered by @umlinks</b>"""
+<b>〽️ Powered by @vijay6142</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Dolby Cinema Auto Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V1</b>
     
-⭕️<b>Creater :</b> @umlinks
+⭕️<b>Creater :</b> @vijay6142
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
