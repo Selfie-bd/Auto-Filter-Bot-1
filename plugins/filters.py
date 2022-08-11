064ef809ddfc6b9f913ad9ea7cf74c99f68d4d18
Selfie SD <webtamil0@gmail.com>
2022-08-11 14:14:39 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -55,7 +55,7 @@ async def filter(client: Bot, message: Message):
                 [InlineKeyboardButton(text="☯Pages 1/1 ☯",callback_data="pages")]
             )
             await message.reply_text(
-                f"<b> Here is the result for You {message.text} Movies
+                f"<b> Here is the result for You {message.text} Movies"
 
 〽️ Powered by @ThiruttuRockers4k</b>",
                 reply_markup=InlineKeyboardMarkup(buttons)
